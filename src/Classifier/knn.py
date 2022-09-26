463f9afc7f427e99276b6103e545cdc29453c4d4
davisp2 <davisp2@illinois.edu>
2022-09-26 16:41:43 -0500

    initilized functions in knn.py

--- src/Classifier/knn.py
+++ src/Classifier/knn.py
@@ -1,1 +1,19 @@
+from . import classify as classify
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
+import pickle 
+
+class KNN(classify.Classifier):
+    def __init__(self, n):
+        self.model = KNeighborsClassifier(n_neighbors=n, weights='distance')
+
+    def train(self, trainingSets): 
+        #TODO:
+        ()
+
+    def classify(self, observation): 
+        #TODO:
+        ()
